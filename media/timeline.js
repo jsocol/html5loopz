58e1b330270a1b823cfd6af9168c1ce326d9ee7a
James Socol <james@mozilla.com>
2011-06-25 00:59:51 -0400

    Make Timeline serializable.

--- media/timeline.js
+++ media/timeline.js
@@ -1,8 +1,12 @@
 var Timeline = (function(CHANNELS) {
+
+    // Insert a sample at a time.
     function TimedSample(sample, time) {
         this.sample = sample;
         this.time = time;
     }
+
+    // Stores a sequence of TimedSamples.
     function Channel() {
         this.samples = [];
     }
@@ -21,11 +25,50 @@ var Timeline = (function(CHANNELS) {
         this.samples.splice(id, 1);
         this.sort();
     };
+    Channel.prototype.moveSample = function(id, newTime) {
+        this.samples[id].time = newTime;
+        this.sort();
+    };
+    Channel.prototype.dumps = function() {
+        var samples = [];
+        for (var i=0; i<this.samples.length; i++) {
+            var sample = this.samples[i];
+            samples.push(sample.sample + ':' + sample.time);
+        }
+        return samples.join(';');
+    };
+    Channel.prototype.loads = function(str) {
+        this.samples = [];
+        var parts = str.split(';');
+        for (var i=0; i<parts.length; i++) {
+            var [s, t] = parts[i].split(':');
+            this.samples.push(new TimedSample(s, t));
+        }
+        this.sort();
+    };
+
+    // Manage a collection of Channels.
     function _Timeline() {
         this.channels = [];
         for (var i = 0; i < CHANNELS; i++) {
             this.channels.push(new Channel());
         }
     }
+    _Timeline.prototype.dumps = function() {
+        var channels = [];
+        for (var i=0; i<this.channels.length; i++) {
+            channels.push(this.channels[i].dumps());
+        }
+        return channels.join('|');
+    };
+    _Timeline.prototype.loads = function(str) {
+        var channels = str.split('|');
+        while (channels.length > this.channels.length) {
+            this.channels.push(new Channel());
+        }
+        for (var i=0; i<channels.length; i++) {
+            this.channels[i].loads(channels[i]);
+        }
+    };
     return new _Timeline();
 })(4);
